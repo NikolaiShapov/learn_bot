9f2dcc28154c83bb642511545d078a7893a9459d
unknown <nikolius@inbox.ru>
2022-09-17 23:18:39 +0300

    add Keyboard Button

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 import logging
 import settings
 from datetime import datetime
@@ -6,17 +7,26 @@ from emoji import emojize
 from glob import glob
 from random import choice, randint
 
-logging.basicConfig(filename="bot.log", level=logging.INFO, encoding='utf-8')
+logging.basicConfig(
+    filename="bot.log",
+    level=logging.INFO,
+    encoding='utf-8')
 
 def greet_user(update, context):
     print("Вызван /start")
     context.user_data['emoji'] = get_smile(context.user_data)
-    update.message.reply_text(f"Здравствуй, пользователь {context.user_data['emoji']}!")
+    my_keyboard = ReplyKeyboardMarkup([['Прислать котика']])
+    update.message.reply_text(
+        f"Здравствуй, пользователь {context.user_data['emoji']}!",
+        reply_markup = main_keyboard(),
+        )
 
 def talk_to_me(update, context):
     context.user_data['emoji'] = get_smile(context.user_data)
     text = update.message.text
-    update.message.reply_text(f"{text} {context.user_data['emoji']}")
+    update.message.reply_text(f"{text} {context.user_data['emoji']}",
+                                reply_markup = main_keyboard(),
+                                )
 
 def get_smile(user_data):
     if 'emoji' not in user_data:
@@ -44,13 +54,24 @@ def guess_number(update, context):
             message = "Введите целое число"
     else:
         message = "Введите число"
-    update.message.reply_text(message)
+    update.message.reply_text(message, reply_markup = main_keyboard())
 
 def send_cat_picture(update, context):
     cat_photo_list = glob('images/cat*.jp*g')
     cat_photo_filename = choice(cat_photo_list)
     chat_id = update.effective_chat.id #берем id чата с текущим пользователем
-    context.bot.send_photo(chat_id=chat_id, photo=open(cat_photo_filename, 'rb'))
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_photo_filename, 'rb'), reply_markup = main_keyboard())
+
+def main_keyboard():
+    return ReplyKeyboardMarkup([['Прислать котика', KeyboardButton('Мои координаты', request_location=True)]])
+
+def user_coordinates(update, context):
+    context.user_data['emoji'] = get_smile(context.user_data)
+    coords = update.message.location
+    update.message.reply_text(
+        f"Ваши координаты {coords} {context.user_data['emoji']}!",
+        reply_markup=main_keyboard()
+    )
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True,)
@@ -59,7 +80,9 @@ def main():
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("guess", guess_number))
     dp.add_handler(CommandHandler("cat", send_cat_picture))
+    dp.add_handler(MessageHandler(Filters.regex('^(Прислать котика)$'), send_cat_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(MessageHandler(Filters.location, user_coordinates))
 
     logging.info(f"{datetime.today()} Бот стартовал")
     mybot.start_polling()
