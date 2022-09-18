7530d33c02feb71fcad8de639e645e74f01dc417
unknown <nikolius@inbox.ru>
2022-09-18 15:14:34 +0300

    Recactoring, readme and keaboard

--- bot.py
+++ bot.py
@@ -1,77 +1,13 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ReplyKeyboardMarkup, KeyboardButton
 import logging
-import settings
 from datetime import datetime
-from emoji import emojize
-from glob import glob
-from random import choice, randint
+from handlers import greet_user, talk_to_me, guess_number, send_cat_picture, user_coordinates
+import settings
 
 logging.basicConfig(
     filename="bot.log",
     level=logging.INFO,
     encoding='utf-8')
-
-def greet_user(update, context):
-    print("Вызван /start")
-    context.user_data['emoji'] = get_smile(context.user_data)
-    my_keyboard = ReplyKeyboardMarkup([['Прислать котика']])
-    update.message.reply_text(
-        f"Здравствуй, пользователь {context.user_data['emoji']}!",
-        reply_markup = main_keyboard(),
-        )
-
-def talk_to_me(update, context):
-    context.user_data['emoji'] = get_smile(context.user_data)
-    text = update.message.text
-    update.message.reply_text(f"{text} {context.user_data['emoji']}",
-                                reply_markup = main_keyboard(),
-                                )
-
-def get_smile(user_data):
-    if 'emoji' not in user_data:
-        smile = choice(settings.USER_EMOJI)
-        return emojize(smile, language='alias')
-    return user_data['emoji']
-
-def play_random_numbers(user_number):
-    bot_number = randint(user_number - 10, user_number + 10)
-    if user_number > bot_number:
-        message = f"Ваше число {user_number}, мое {bot_number}, вы выиграли"
-    elif user_number == bot_number:
-        message = f"Ваше число {user_number}, мое {bot_number}, ничья"
-    else:
-        message = f"Ваше число {user_number}, мое {bot_number}, вы проиграли"
-    return message
-
-def guess_number(update, context):
-    print(context.args)
-    if context.args:
-        try:
-            user_number = int(context.args[0])
-            message = play_random_numbers(user_number)
-        except (TypeError, ValueError):
-            message = "Введите целое число"
-    else:
-        message = "Введите число"
-    update.message.reply_text(message, reply_markup = main_keyboard())
-
-def send_cat_picture(update, context):
-    cat_photo_list = glob('images/cat*.jp*g')
-    cat_photo_filename = choice(cat_photo_list)
-    chat_id = update.effective_chat.id #берем id чата с текущим пользователем
-    context.bot.send_photo(chat_id=chat_id, photo=open(cat_photo_filename, 'rb'), reply_markup = main_keyboard())
-
-def main_keyboard():
-    return ReplyKeyboardMarkup([['Прислать котика', KeyboardButton('Мои координаты', request_location=True)]])
-
-def user_coordinates(update, context):
-    context.user_data['emoji'] = get_smile(context.user_data)
-    coords = update.message.location
-    update.message.reply_text(
-        f"Ваши координаты {coords} {context.user_data['emoji']}!",
-        reply_markup=main_keyboard()
-    )
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True,)
